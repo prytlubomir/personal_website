e37d3a0512b1d3da6dd41bc6f8ef1ba16278420f
Liubomyr Pryt <prytlubomyr@gmail.com>
2026-06-26 15:32:00 +0100

    Added character estimation to this branch.

--- script.js
+++ script.js
@@ -1,5 +1,26 @@
 let background = document.getElementById('background-texture');
 
+
+function getStyle(element, style) {
+    return window.getComputedStyle(element)[style]
+}
+
+function pxToCh(px) {
+    const chLengthEstimation = 8; // 8px
+    let chValue = px / chLengthEstimation;
+    return chValue;
+}
+
+function pxToLineHeight(px, element) {
+    const lineHeightEm = 1.2; // "normal" value in em, according to MDN
+    let fontFize = parseFloat(getStyle(element, "font-size"));
+    let lineHeight = fontFize * lineHeightEm;
+    let emValue = px / lineHeight;
+    return emValue;
+}
+
+
+
 function selectRandom(arr) {
     return arr[Math.floor(Math.random() * arr.length)]
 }
@@ -19,11 +40,26 @@ function generateBackground(lenght) {
             let ind = p.indexOf(char);
             p.splice(ind, 1);
         } else {
-            console.log('else');
             p = Array.from(charList);
         }
     }
     return result;
 }
 
-setInterval(() => { background.innerText = generateBackground(3000) }, 200);
+
+function estimateCharacterCount(element) {
+    let bgw = parseFloat(getStyle(element, 'width'));
+    let bgh = parseFloat(getStyle(element, 'height'));
+
+    let bch = pxToCh(bgw);
+    let blh = pxToLineHeight(bgh, element);
+
+    let total = bch * blh;
+
+    return total;
+}
+
+
+let bgTotalCharacters = estimateCharacterCount(background);
+window.onresize = () => { bgTotalCharacters = estimateCharacterCount(background) };
+setInterval(() => { background.innerText = generateBackground(bgTotalCharacters) }, 200);
