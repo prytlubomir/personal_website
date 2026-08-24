c4282d604a7453a6c0c72ad632d48a190d15a4f0
Liubomyr Pryt <prytlubomyr@gmail.com>
2026-08-24 07:00:50 +0100

    Finished project card component.

--- src/components/ProjectsSection.vue
+++ src/components/ProjectsSection.vue
@@ -2,49 +2,73 @@
     <AppSection class="projects" id="projects">
         <ContentLimiter>
             <SectionHeading>My projects</SectionHeading>
-            <ProjectCard src="/logov5.svg" projectName="Personal website">
-                My website made with Vue.js v3.
-                You're using it right now.
-            </ProjectCard>
-            <!-- <div class="content">
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Necessitatibus culpa quidem explicabo nihil
-                assumenda fugiat, odio modi earum iusto commodi iure at, harum veniam in, facilis cum neque? Eius,
-                natus!
-                Repellat delectus quibusdam nobis itaque eaque magnam optio ab ad nostrum nulla quos eveniet
-                consequatur,
-                assumenda sequi ut illo accusamus. Quam quaerat dolore itaque consequatur animi voluptates veritatis
-                earum
-                labore?
-                Officia quidem in blanditiis error atque minus harum quam quod sit, quos ab voluptates sint quia
-                incidunt
-                dolor facilis veritatis nemo delectus officiis obcaecati! Ducimus quaerat tempore magnam beatae impedit.
-                Fugit sint molestiae officiis consequuntur esse distinctio natus totam optio. Qui ab esse, unde libero
-                perferendis enim illo nam odit minus, harum facilis, incidunt molestiae iusto? Praesentium in
-                dignissimos
-                voluptas.
-                Repudiandae quisquam alias soluta ipsa deserunt nam iure, dolorem officia cupiditate, itaque tempore
-                explicabo doloribus sed nisi magni amet. Beatae laborum quod dignissimos voluptatem non nulla facere
-                quasi
-                magnam voluptate.
-                Facere sequi tempore facilis maiores similique illo neque quod, tempora temporibus odit vitae veritatis
-                laudantium nisi necessitatibus quas corrupti hic sed earum eius obcaecati molestiae a nam! Vero, iure
-                error.
-                Rem dicta recusandae quia necessitatibus, placeat eum magnam perspiciatis expedita est sed fugiat
-                temporibus
-                sequi veniam ducimus commodi tempore! Asperiores architecto in quam autem numquam similique, voluptatem
-                corporis a explicabo!
-                Dolore quia ipsa exercitationem tempora quibusdam quas, unde nesciunt? Perspiciatis ducimus repudiandae
-                quibusdam dolor similique illum, deserunt eaque nulla placeat voluptatibus eligendi amet aspernatur hic
-                impedit blanditiis, ea libero modi?
-                Dolores sequi incidunt reiciendis laborum officiis inventore. Earum sed ex, numquam fugit similique,
-                dolore
-                laborum recusandae facere debitis culpa beatae optio laudantium aliquid? Placeat necessitatibus sit nam!
-                Reiciendis, veniam alias!
-                Quaerat in vitae doloremque. Libero, similique! Laborum praesentium impedit tempora explicabo illo
-                commodi.
-                Itaque est laboriosam voluptatem, inventore error libero pariatur quia unde harum voluptatibus id! Vel
-                molestiae aspernatur dolor?
-            </div> -->
+            <div class="project-cards">
+                <ProjectCard src="/logov5.svg" projectName="Personal website" imgType="icon">
+                    My website made with Vue.js v3.
+                    You're using it right now.
+                </ProjectCard>
+                <ProjectCard src='/icon.svg' projectName="Crop Studio" imgType="icon">
+                    A convenient batch-cropping tool for images.
+                </ProjectCard>
+                <ProjectCard src='/poster.png' projectName="Lorem ipsum">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                </ProjectCard>
+                <ProjectCard src='/gui.png' projectName="Lorem ipsum">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                </ProjectCard>
+                <ProjectCard src='/tui.png' projectName="Lorem ipsum">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                </ProjectCard>
+                <ProjectCard src="/logov5.svg" projectName="Personal website" imgType="icon">
+                    My website made with Vue.js v3.
+                    You're using it right now.
+                </ProjectCard>
+                <ProjectCard src='/icon.svg' projectName="Crop Studio" imgType="icon">
+                    A convenient batch-cropping tool for images.
+                </ProjectCard>
+                <ProjectCard src='/poster.png' projectName="Lorem ipsum">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                </ProjectCard>
+                <ProjectCard src='/gui.png' projectName="Lorem ipsum">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                </ProjectCard>
+                <ProjectCard src='/tui.png' projectName="Lorem ipsum">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                </ProjectCard>
+                <ProjectCard src="/logov5.svg" projectName="Personal website" imgType="icon">
+                    My website made with Vue.js v3.
+                    You're using it right now.
+                </ProjectCard>
+                <ProjectCard src='/icon.svg' projectName="Crop Studio" imgType="icon">
+                    A convenient batch-cropping tool for images.
+                </ProjectCard>
+                <ProjectCard src='/poster.png' projectName="Lorem ipsum">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                </ProjectCard>
+                <ProjectCard src='/gui.png' projectName="Lorem ipsum">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                </ProjectCard>
+                <ProjectCard src='/tui.png' projectName="Lorem ipsum">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                </ProjectCard>
+                <ProjectCard src="/logov5.svg" projectName="Personal website" imgType="icon">
+                    My website made with Vue.js v3.
+                    You're using it right now.
+                </ProjectCard>
+                <ProjectCard src='/icon.svg' projectName="Crop Studio" imgType="icon">
+                    A convenient batch-cropping tool for images.
+                </ProjectCard>
+                <ProjectCard src='/poster.png' projectName="Lorem ipsum">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                </ProjectCard>
+                <ProjectCard src='/gui.png' projectName="Lorem ipsum">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                </ProjectCard>
+                <ProjectCard src='/tui.png' projectName="Lorem ipsum">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                </ProjectCard>
+            </div>
+            <div style="width: 1ex; height: 1ex; background: red;"></div>
         </ContentLimiter>
     </AppSection>
 </template>
@@ -66,4 +90,16 @@
     }
 </script>
 
-<style></style>
+<style>
+
+.project-cards {
+    --card-width: 16.5rem;
+    display: grid;
+    gap: 3rem;
+    justify-content: center;
+    grid-template-columns: repeat(auto-fit, 16.5rem);
+    /*flex-wrap: wrap;*/
+    /*flex-direction: row;*/
+}
+
+</style>
