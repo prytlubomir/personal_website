ea7be3cec2feeb8ba4436def99393a4d65f9f2e6
Liubomyr Pryt <prytlubomyr@gmail.com>
2026-09-21 03:17:34 +0100

    Added a component that is a button with most of the styles removed

---
+++ src/components/TextButton.vue
@@ -0,0 +1,33 @@
+<template>
+<button class="text-button" ref="textButton">
+	<slot></slot>
+</button>
+</template>
+<script>
+
+    export default {
+        name: "TextButton",
+    }
+</script>
+<style scoped>
+
+    .text-button {
+        display: block;
+        box-shadow: none;
+        border: none;
+        outline: none;
+        background: none;
+        padding: 0;
+        font-size: 1rem;
+        font-weight: normal;
+        line-height: 1.2rem;
+        color: #ddd;
+        text-align: center;
+    }
+
+    .text-button:hover {
+        color: white !important;
+        /*text-decoration: underline;*/
+    }
+
+</style>
