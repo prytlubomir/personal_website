7f1ffa4f7088be9b0ff72d1cc5704824d72bae43
Liubomyr Pryt <prytlubomyr@gmail.com>
2026-09-25 23:37:07 +0100

    Added styles to the DisclosureControl

--- src/components/DisclosureControls.vue
+++ src/components/DisclosureControls.vue
@@ -47,14 +47,58 @@
 </script>
 <style scoped>
 
-    .min-link {
-        text-decoration: none;
-        color: #aaaaaa;
+    .content-controls {
+        box-sizing: content-box;
+        position: relative;
+        /*height: 1.2rem;*/
+        /*padding: 1rem 0;*/
+        margin-top: 1rem;
     }
 
-    .min-link:hover {
-        text-decoration: underline;
-        color: #f8f8f8;
+    .content-controls .controls {
+        position: relative;
+        display: flex;
+        z-index: 1;
+        justify-content: space-between;
+        flex-wrap: wrap;
     }
 
+    .content-controls .show-button {
+        margin: 1rem 0;
+    }
+
+    .content-controls .show {
+        display: flex;
+        flex-wrap: wrap;
+        gap: 0 3rem;
+    }
+
+    /*.content-controls .show-button {
+    position: relative;
+}
+
+.content-controls .show-button::after{
+    content: '';
+    position: absolute;
+    left: 0;
+    right: 0;
+    margin-top: 1em;
+    height: 1px;
+    width: inherit;
+}
+
+.content-controls .show-button:hover::after{
+    background-color: currentColor;
+}*/
+
+    .content-controls .background {
+        position: absolute;
+        width: 100%;
+        /*height: 3.2rem;*/
+        height: 100%;
+        top: 0;
+        left: calc(var(--content-limiter-padding) * -1);
+        z-index: 0;
+        background-color: #000;
+    }
 </style>
